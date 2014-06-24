34b30bdbc5684cc30b0671652d39d52adb4e0488
Bentroudi Galaad <ons.darkside@sfr.fr>
2014-06-24 12:00:58 +0200

    .

--- src/game.cpp
+++ src/game.cpp
@@ -38,7 +38,8 @@ int Game::initContext()
 
 	// OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	//glfwWindowHint(GLFW_SAMPLES, 8);
